80cff788c56a10a2ec9dd686f95909685a8dac71
yoonseo <cysyes0403@naver.com>
2025-12-18 16:18:52 +0900

    feat: 도전 기능 구현

--- main.cpp
+++ main.cpp
@@ -15,7 +15,14 @@ public:
     void PrintInfo() const {
         cout << "name: " << name_ << ", price: " << price_ << endl;
     }
+    int GetPrice() const {
+        return price_;
+    }
 };
+template <typename T>
+bool compareItemsByPrice(const T& a, const T& b) {
+    return a.GetPrice() < b.GetPrice();
+}
 
 template <typename T>
 class Inventory {
@@ -32,18 +39,31 @@ public:
             capacity_ = capacity;
         pItems_ = new T[capacity_];
     }
+    Inventory(const Inventory<T>& other) {  //복사 생성자
+        capacity_ = other.capacity_;
+        size_ = other.size_;
+        pItems_ = new T[capacity_];
+        for (int i = 0; i < other.size_; i++) {
+            pItems_[i] = other.pItems_[i];
+        }
+        cout << "*Inventory Copy!"<<endl;
+    }
     ~Inventory() {
         delete[] pItems_;
         pItems_ = nullptr;
     }
     void AddItem(const T& item) {
-        if (size_ < capacity_) {
-            pItems_[size_] = item; //아이템 넣기
-            size_++;
+        if (size_ >= capacity_) {
+            //cout << "*Inventory is full..." << endl;
+            cout << "*Inventory Size Up" << endl;
+            T* temp = new T[capacity_ * 2];
+            copy(pItems_, pItems_ + size_, temp);
+            delete[] pItems_;
+            capacity_ *= 2;
+            pItems_ = temp;
         }
-        else {
-            cout << "*Inventory is full..." << endl;
-        }
+        pItems_[size_] = item; //아이템 넣기
+        size_++;
     }
     void RemoveLastItem() {
         if (size_ == 0) {
@@ -69,6 +89,25 @@ public:
             }
         }
     }
+    void Assign(const Inventory<T>& other) {
+        size_ = other.size_;
+        capacity_ = other.capacity_;
+        delete[] pItems_;
+        pItems_ = new T[size_];
+        for (int i = 0; i < size_; i++) {
+            pItems_[i] = other.pItems_[i];
+        }
+        cout << "*Inventory Overwrite!" << endl;
+    }
+    void Resize(int newCapacity) {
+        T* temp = new T[newCapacity];
+        copy(pItems_, pItems_ + size_, temp);
+        delete[] pItems_;
+        pItems_ = temp;
+    }
+    void SortItems() {
+        sort(pItems_, pItems_ + size_,compareItemsByPrice<T>);
+    }
 };
 
 int main() {
@@ -87,6 +126,9 @@ int main() {
     box.printAllItems();
     box.AddItem(apple);
     box.printAllItems();
+    box.SortItems();
+    box.printAllItems();
+
 
     return 0;
 }
