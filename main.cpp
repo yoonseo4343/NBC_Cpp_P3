0d6ac61c0cf6bd1b6a6d36bcbc5b6eed04e0f162
yoonseo <cysyes0403@naver.com>
2025-12-18 17:37:48 +0900

    test: 기능 테스트

--- main.cpp
+++ main.cpp
@@ -18,6 +18,9 @@ public:
     int GetPrice() const {
         return price_;
     }
+    string GetName() const {
+        return name_;
+    }
 };
 template <typename T>
 bool compareItemsByPrice(const T& a, const T& b) {
@@ -43,7 +46,7 @@ public:
         capacity_ = other.capacity_;
         size_ = other.size_;
         pItems_ = new T[capacity_];
-        for (int i = 0; i < other.size_; i++) {
+        for (int i = 0; i < other.size_; ++i) {
             pItems_[i] = other.pItems_[i];
         }
         cout << "*Inventory Copy!"<<endl;
@@ -55,13 +58,14 @@ public:
     void AddItem(const T& item) {
         if (size_ >= capacity_) {
             //cout << "*Inventory is full..." << endl;
-            cout << "*Inventory Size Up" << endl;
+            cout << "*Upgrade Inventory" << endl;
             T* temp = new T[capacity_ * 2];
             copy(pItems_, pItems_ + size_, temp);
             delete[] pItems_;
             capacity_ *= 2;
             pItems_ = temp;
         }
+        cout << "Add: " << item.GetName() << endl;
         pItems_[size_] = item; //아이템 넣기
         size_++;
     }
@@ -70,6 +74,7 @@ public:
             cout << "*Inventory is empty..." << endl;
         }
         else {
+            cout << "*Remove Last Item" << endl;
             size_--;
         }
     }
@@ -80,6 +85,7 @@ public:
         return capacity_;
     }
     void printAllItems() const {
+        cout << "=====Item List==================" << endl;
         if (size_ == 0) {
             cout << "*Empty..." << endl;
         }
@@ -88,6 +94,7 @@ public:
                 pItems_[i].PrintInfo(); //각 아이템 멤버 함수 출력
             }
         }
+        cout << "================================" << endl;
     }
     void Assign(const Inventory<T>& other) {
         size_ = other.size_;
@@ -104,8 +111,11 @@ public:
         copy(pItems_, pItems_ + size_, temp);
         delete[] pItems_;
         pItems_ = temp;
+        capacity_ = newCapacity;
+        cout << "Resize: "<<GetCapacity() << endl;
     }
     void SortItems() {
+        cout << "*Sort by Price..." << endl;
         sort(pItems_, pItems_ + size_,compareItemsByPrice<T>);
     }
 };
@@ -116,8 +126,9 @@ int main() {
     Item banana = Item("banana", 100);
     Item apple = Item("apple", 150);
 
-    Inventory<Item> box;
-    cout << "상자크기: " << box.GetCapacity() << endl;
+    Inventory<Item> box=Inventory<Item>(-3);
+    cout << "Inventory Size: " << box.GetCapacity() << endl;
+    box.printAllItems();
     box.AddItem(cap);
     box.AddItem(shoes);
     box.AddItem(banana);
@@ -126,9 +137,15 @@ int main() {
     box.printAllItems();
     box.AddItem(apple);
     box.printAllItems();
-    box.SortItems();
+    box.SortItems(); // 정렬
     box.printAllItems();
 
-
+    Inventory<Item> box2 = Inventory<Item>(box); // 복사
+    box2.printAllItems();
+    box2.Resize(10); // 사이즈 조절
+    
+    Inventory<Item> box3 = Inventory<Item>(5);
+    box3.Assign(box2); // 덮기
+    box3.printAllItems();
     return 0;
 }
